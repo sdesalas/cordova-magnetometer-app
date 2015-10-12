8b6b12511830611b44fab58d84e8ba18b9efa998
Steven de Salas <steven@desalasworks.com>
2015-10-13 01:43:52 +1100

    Moving cordova to CLI project structure

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-magnetometer/www/magnetometer.js",
+        "id": "cordova-plugin-magnetometer.main",
+        "clobbers": [
+            "cordova.plugins.magnetometer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-vibration": "1.2.0",
+    "cordova-plugin-magnetometer": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
